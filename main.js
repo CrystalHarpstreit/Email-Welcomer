32fff405ccf2a70b8702752e6f9278e28ec2bad8
Crystal <crystal.harpstreit@gmail.com>
2014-12-26 21:55:30 -0800

    Added button

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ var container = document.querySelector('.container');
 
 function myTime() {
   var date = new Date();
-  var hours = date.getHours() - 12;
+  var hours = date.getHours();
   var minutes = date.getMinutes();
 
-  document.querySelector('.time').innerHTML = '' + hours + ':' + minutes;
+  if (hours < 12) {
+    document.querySelector('.time').innerHTML = '' + hours + ':' + minutes;
+  } else {
+    var hours = date.getHours() - 12;
+    document.querySelector('.time').innerHTML = '' + hours + ':' + minutes;
+  }
 
   setTimeout(myTime, 1000);
 }
